ba33de88d5762273b3c3b37e31c6c9fc0d66e4bf
dkilcy <david@kilcyconsulting.com>
2013-10-26 20:11:54 -0400

    Change master from cubietruck3-1 to mintbox2-1

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -10,7 +10,7 @@ export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 # export HADOOP_CLASSPATH=
 
 # The maximum amount of heap to use, in MB. Default is 1000.
-export HADOOP_HEAPSIZE=256
+export HADOOP_HEAPSIZE=1000
 
 # Extra Java runtime options.  Empty by default.
 export HADOOP_OPTS=-server
